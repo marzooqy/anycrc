0a22f3aad0fc97f4e7938f9ee00583d0839319f6
marzooqy <marzooqy@users.noreply.github.com>
2024-04-13 22:44:55 +0300

    Increase version number

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, Extension
 
 setup(
     name = 'anycrc',
-    version = '0.2.1',
+    version = '0.2.2',
     ext_modules = [Extension(name='anycrc.anycrc', sources=['src/anycrc/anycrc.pyx', 'lib/crcany/model.c', 'lib/crcany/crc.c'])]
 )
