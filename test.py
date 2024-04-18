29c304f23d74d7cf43ce1c248155cfce3ee43850
marzooqy <marzooqy@users.noreply.github.com>
2024-04-18 19:09:25 +0300

    More comprehensive test

--- test.py
+++ test.py
@@ -4,29 +4,55 @@ import sys
 word_size = 64 if sys.maxsize > 2 ** 32 else 32
 
 #The standard test data b'123456789' isn't good enough when testing slice-by-16 because the data length is < 16
-test_data = b'abcdefghijklmnopqrstuvwxyz'
+test_data = b'123456789'
+test_data2 = b'abcdefghijklmnopqrstuvwxyz'
 
 for model, params in anycrc.models.items():
     if word_size == 32 and params.width > 32:
         break
         
-    crc = anycrc.Model(model)
+    if params.width % 4 == 0:
+        disp_width = params.width // 4
+    else:
+        disp_width = params.width // 4 + 1
+        
+    fmt_str = '0x{:0widthx} 0x{:0widthx}'.replace('width', str(disp_width))
+    
+    print(model)
+    
+    crc = anycrc.Model(model)   
     crc2 = anycrc.Model(model)
     
     #read all at once
-    value = crc.calc(test_data)
-    value2 = crc2._calc(test_data)
+    value = crc._calc(test_data)
+    check = params.check
     
-    print(model)
-    print(value, value2)
-    print()
-    
-    assert value == value2
-    
-    #read one char at a time
+    print('1 byte whole:     ' + fmt_str.format(value, check))
+    assert value == check
     crc.reset()
     
+    #read one char at a time
     for c in test_data:
-        value = crc.calc(c.to_bytes(1, 'little'))
+        value = crc._calc(c.to_bytes(1, 'little'))
     
+    print('1 byte partial:   ' + fmt_str.format(value, check))
+    assert value == check
+    
+    crc.reset()
+    crc2.reset()
+    
+    #read all at once
+    value = crc.calc(test_data2)
+    value2 = crc2._calc(test_data2)
+
+    print('16 bytes whole:   ' + fmt_str.format(value, value2))
     assert value == value2
+    crc.reset()
+    
+    #read one char at a time
+    for c in test_data2:
+        value = crc.calc(c.to_bytes(1, 'little'))
+
+    print('16 bytes partial: ' + fmt_str.format(value, value2))
+    assert value == value2
+    print()
