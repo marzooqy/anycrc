fada19f0888a1f8397a9382e87094b8c747dc124
marzooqy <marzooqy@users.noreply.github.com>
2024-04-18 16:45:03 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, Extension
 
 setup(
     name = 'anycrc',
-    version = '0.3.0',
+    version = '0.3.1',
+    package_dir = {"": "src"},
     ext_modules = [Extension(name='anycrc.anycrc', sources=['src/anycrc/anycrc.pyx', 'lib/crcany/model.c', 'lib/crcany/crc.c'])]
 )
