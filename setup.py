b557bec55dc8d2864196537342db026b94db277f
marzooqy <marzooqy@users.noreply.github.com>
2024-04-21 19:05:18 +0300

    Remove cython import

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, Extension
-from Cython.Build import cythonize
+
 setup(
     name = 'anycrc',
     version = '0.3.3',
     package_dir = {"": "src"},
-    ext_modules = cythonize([Extension(name='anycrc.anycrc', sources=['src/anycrc/anycrc.pyx', 'lib/crcany/model.c', 'lib/crcany/crc.c'])])
+    ext_modules = [Extension(name='anycrc.anycrc', sources=['src/anycrc/anycrc.pyx', 'lib/crcany/model.c', 'lib/crcany/crc.c'])]
 )
