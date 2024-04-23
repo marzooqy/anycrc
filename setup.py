a5afacb0dbc888b5534760fe10a15add37e8a521
marzooqy <marzooqy@users.noreply.github.com>
2024-04-23 21:07:10 +0300

    Attempt to fix linux error 5

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup, Extension
-from Cython.Build import cythonize
+import sys
 
+compile_args = [] 
+
+if sys.platform == 'win32':
+    compile_args = ['-openmp']
+else:
+    compile_args = ['-fopenmp']
+    
 setup(
     name = 'anycrc',
-    version = '0.4.0',
+    version = 0.4.0,
     package_dir = {"": "src"},
     
     ext_modules = [
         Extension(
             name='anycrc.anycrc',
-            extra_compile_args=['-openmp', '-fopenmp'],
+            extra_compile_args=compile_args,
             sources=['src/anycrc/anycrc.pyx', 'lib/crcany/model.c', 'lib/crcany/crc.c']
         )
     ]
