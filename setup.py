9e932048ad37d061a5c7cc87e2c3a3709a2ff123
Hussain Al Marzooq <marzooqy@users.noreply.github.com>
2024-05-08 06:45:51 +0300

    Fix parallelism on Windows

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 import sys
 
+class Build(build_ext):
+    def finalize_options(self):
+        super().finalize_options()
+        
+        #gcc optimizes the slice-by-16 algorithm better
+        if sys.platform == 'win32':
+            self.compiler = "mingw32"
+            
 compile_args = ['-fopenmp', '-O2']
 link_args = ['-fopenmp', '-O2']
 
@@ -15,9 +24,9 @@ if sys.platform == 'win32':
                 
 setup(
     name = 'anycrc',
-    version = '0.6.0',
+    version = '0.6.1',
     package_dir = {"": "src"},
-    
+    cmdclass={"build_ext": Build},
     ext_modules = [
         Extension(
             name='anycrc.anycrc',
