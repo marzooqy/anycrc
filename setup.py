5e6c863c828ccc45144f959c0ff35fb520becda5
Hussain Al Marzooq <marzooqy@users.noreply.github.com>
2024-05-08 15:13:35 +0300

    Fix compilation on Windows 3

--- setup.py
+++ setup.py
@@ -2,29 +2,36 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import sys
 
+is_64bit = sys.maxsize > 2 ** 32
+
 class Build(build_ext):
     def finalize_options(self):
         super().finalize_options()
         
         #gcc optimizes the slice-by-16 algorithm better
-        if sys.platform == 'win32':
+        if sys.platform == 'win32' and is_64bit:
             self.compiler = "mingw32"
             
-compile_args = ['-fopenmp', '-O2']
-link_args = ['-fopenmp', '-O2']
-
 if sys.platform == 'win32':
-    link_args = ['-static-libgcc',
-                 '-static-libstdc++',
-                 '-Wl,-Bstatic,--whole-archive',
-                 '-lwinpthread',
-                 '-Wl,--no-whole-archive',
-                 '-fopenmp',
-                 '-O2']
-                 
-    if sys.maxsize > 2 ** 32:
-        compile_args.append('-DMS_WIN64')
+    if is_64bit:
+        compile_args = ['-fopenmp', '-O2','-DMS_WIN64']
+        link_args = ['-static-libgcc',
+                     '-static-libstdc++',
+                     '-Wl,-Bstatic,--whole-archive',
+                     '-lwinpthread',
+                     '-Wl,--no-whole-archive',
+                     '-fopenmp',
+                     '-O2',
+                     '-DMS_WIN64']
+                     
+    else:
+        compile_args = ['/openmp']
+        link_args = ['/openmp']
         
+else:
+    compile_args = ['-fopenmp', '-O2']
+    link_args = ['-fopenmp', '-O2']
+    
 setup(
     name = 'anycrc',
     version = '0.6.1',
